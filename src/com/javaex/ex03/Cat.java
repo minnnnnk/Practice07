f34be3abd9f22d993ec9c48cd138a28a78bb1af5
minnnk <jubal9153@gmail.com>
2022-04-28 16:46:18 +0900

    최종

--- src/com/javaex/ex03/Cat.java
+++ src/com/javaex/ex03/Cat.java
@@ -2,14 +2,11 @@ package com.javaex.ex03;
 
 public class Cat implements Soundable {
 	
-	public Cat() {
-	}
-	public Cat(String sound) {
-	}
+
 
 	public String sound() {
-		String sound = "야옹";
-		return sound;
+		
+		return "야옹";
 	}
 
 
